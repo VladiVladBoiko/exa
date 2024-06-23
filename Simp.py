75656ee2e9b6aa4e1fd143b8a1d80f128c909e30
Владислав <moonerboiko@yandex.ru>
2024-06-23 16:10:47 +0300

    Add files via upload

---
+++ Simp.py
@@ -0,0 +1,16 @@
+
+#Функция в функции
+#Ввод числа
+a = int( input( "Введите число: "))
+#Функции
+def positive():
+    return ("Число " + str(a) +" положительное" )
+def negative():
+    return ("Число " + str(a) +" отрицательное" )
+def test():
+    if a>=0:
+        return positive()
+    else:
+        return negative()
+#Вывод результата
+print( test() )
